d0259b52643f31e25c6d8e0ed2103b1d0977d1be
EC2 Default User <ec2-user@ip-172-31-15-221.ap-northeast-1.compute.internal>
2021-08-10 03:26:43 +0000

    Update create action for successful user registration

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      
+      flash[:success] = "Welcome! You have signed up successfully."
+      redirect_to @user
     else
       render 'new'
     end
