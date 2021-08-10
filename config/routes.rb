ee02691aa53af5f8790125f19498b0dcadaf8df0
EC2 Default User <ec2-user@ip-172-31-15-221.ap-northeast-1.compute.internal>
2021-08-10 07:15:40 +0000

    Update header links

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'sessions/new'
   root 'static_pages#home'
-  get '/signup', to: 'users#new'
+  get 'users/sign_up', to: 'users#new'
   get '/users/sign_in', to: 'sessions#new'
   post '/users/sign_in', to: 'sessions#create'
   post 'logout', to: 'sessions#destroy'
