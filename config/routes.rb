6b1c22f2ded701471f9b9d0a31c4a0f1d838f72b
EC2 Default User <ec2-user@ip-172-31-15-221.ap-northeast-1.compute.internal>
2021-08-10 05:58:50 +0000

    Update sessions new view and change routing

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   get 'sessions/new'
   root 'static_pages#home'
   get '/signup', to: 'users#new'
-  get '/login', to: 'sessions#new'
-  post '/login', to: 'sessions#create'
+  get '/users/sign_in', to: 'sessions#new'
+  post '/users/sign_in', to: 'sessions#create'
   post 'logout', to: 'sessions#destroy'
   resources :users
 end
