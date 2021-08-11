2f1b6f10a7db029d461d5373cf68af3aa1da3f72
EC2 Default User <ec2-user@ip-172-31-15-221.ap-northeast-1.compute.internal>
2021-08-11 06:01:56 +0000

    Add users_login_test with valid information

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -2,6 +2,22 @@ require "test_helper"
 
 class UsersLoginTest < ActionDispatch::IntegrationTest
   
+  def setup
+    @user = users(:michael)
+  end
+  
+  test "login with valid information" do
+    get users_sign_in_path
+    post users_sign_in_path, params: { session: { email: @user.email,
+                                password: 'password' } }
+    assert_redirected_to root_url
+    follow_redirect!
+    assert_template 'static_pages/home'
+    assert_select "a[href=?]", users_sign_in_path, count: 0
+    assert_select "a[href=?]", logout_path
+    assert_select "a[href=?]", user_path(@user)
+  end
+  
   test "login with invalid information" do
     get users_sign_in_path
     assert_template 'sessions/new'
