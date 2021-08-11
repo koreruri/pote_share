ce86c994aafee4eb05d0980c0f04cf19289526ee
EC2 Default User <ec2-user@ip-172-31-15-221.ap-northeast-1.compute.internal>
2021-08-11 06:35:05 +0000

    Add users_logout_test

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -10,28 +10,38 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     get users_sign_in_path
     post users_sign_in_path, params: { session: { email: @user.email,
                                 password: "invalid" } }
+    assert_not is_logged_in?
     assert_template 'sessions/new'
     assert_not flash.empty?
     get root_path
     assert flash.empty?
   end
   
-  test "login with valid information" do
+  test "login with valid information followed by logout" do
     get users_sign_in_path
     post users_sign_in_path, params: { session: { email: @user.email,
                                 password: 'password' } }
+    assert is_logged_in?
     assert_redirected_to root_url
     follow_redirect!
     assert_template 'static_pages/home'
     assert_select "a[href=?]", users_sign_in_path, count: 0
     assert_select "a[href=?]", logout_path
     assert_select "a[href=?]", user_path(@user)
+    delete logout_path
+    assert_not is_logged_in?
+    assert_redirected_to root_url
+    follow_redirect!
+    assert_select "a[href=?]", users_sign_in_path
+    assert_select "a[href=?]", logout_path, count: 0
+    assert_select "a[href=?]", user_path(@user), count: 0
   end
   
   test "login with invalid information" do
     get users_sign_in_path
     assert_template 'sessions/new'
     post users_sign_in_path, params: { session: { email: "", password: "" } }
+    assert_not is_logged_in?
     assert_template 'sessions/new'
     assert_not flash.empty?
     get root_path
