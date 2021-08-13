8608e00cd2e6ae6e5986e7f28e9b3179eb76d173
EC2 Default User <ec2-user@ip-172-31-15-221.ap-northeast-1.compute.internal>
2021-08-13 03:12:41 +0000

    Add authorization to users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:show, :edit, :update]
+  
   def new
     @user = User.new
   end
@@ -47,4 +49,15 @@ class UsersController < ApplicationController
       params.require(:user).permit(:name, :email, :password,
                                   :password_confirmation)
     end
+    
+    #beforeアクション
+    
+    #ログイン済みのユーザーかどうか確認
+    def logged_in_user
+      unless logged_in?
+        flash[:danger] = "You need to sign in or sign up before continuing."
+        redirect_to users_sign_in_path
+      end
+    end
+    
 end
