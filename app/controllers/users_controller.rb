02a40c6d5d246afe1d4579ee9d7120744efa7ce9
EC2 Default User <ec2-user@ip-172-31-15-221.ap-northeast-1.compute.internal>
2021-08-13 08:03:56 +0000

    Add users profile routing, action, and view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,6 +20,10 @@ class UsersController < ApplicationController
     @user = current_user
   end
   
+  def profile
+    @user = current_user
+  end
+  
   def edit
     @user = current_user
   end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root 'static_pages#home'
   get 'users/sign_up', to: 'users#new'
   get 'users/account', to: 'users#show'
+  get 'users/profile', to: 'users#profile'
   get 'users/edit', to: 'users#edit'
   patch 'users/edit', to: 'users#update'
   get '/users/sign_in', to: 'sessions#new'
