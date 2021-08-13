ea26a4ef57cce4ccee753301e86f553e5cc52114
EC2 Default User <ec2-user@ip-172-31-15-221.ap-northeast-1.compute.internal>
2021-08-13 03:23:42 +0000

    Add authorization to users test to users_controller_test

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,8 +1,31 @@
 require "test_helper"
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
+  
+  def setup
+    @user = users(:michael)
+  end
+  
   test "should get new" do
     get users_sign_up_path
     assert_response :success
+  end
+  
+  test "should redirect show when not logged in" do
+    get users_account_path
+    assert_not flash.empty?
+    assert_redirected_to users_sign_in_url
+  end
+  
+  test "should redirect edit when not logged in" do
+    get users_edit_path
+    assert_not flash.empty?
+    assert_redirected_to users_sign_in_url
+  end
+  
+  test "should redirect update when not logged in" do
+    patch users_edit_path, params: { user: { email: @user.email } }
+    assert_not flash.empty?
+    assert_redirected_to users_sign_in_url
   end
 end
