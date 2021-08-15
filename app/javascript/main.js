0d9b7a0f120eaa724496930e2168f8368512469f
EC2 Default User <ec2-user@ip-172-31-15-221.ap-northeast-1.compute.internal>
2021-08-15 09:36:54 +0000

    Update edit view css with errors

--- app/javascript/main.js
+++ app/javascript/main.js
@@ -1,3 +1,6 @@
 $(function() {
-  $("div.field_with_errors:has(label)").addClass("parent");
+  if ($(location).attr('pathname') == "/users/edit") {
+    $("div.field_with_errors:has(label)").addClass("parent edit");
+    $("div.field_with_errors:has(input)").addClass("edit");
+  }
 });
