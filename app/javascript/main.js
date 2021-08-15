4f416c4a58aee734f06fae703c62d5e778d02f58
EC2 Default User <ec2-user@ip-172-31-15-221.ap-northeast-1.compute.internal>
2021-08-15 10:04:11 +0000

    Update profile  view css with errors

--- app/javascript/main.js
+++ app/javascript/main.js
@@ -2,5 +2,10 @@ $(function() {
   if ($(location).attr('pathname') == "/users/edit") {
     $("div.field_with_errors:has(label)").addClass("parent edit");
     $("div.field_with_errors:has(input)").addClass("edit");
+  } else if ($(location).attr('pathname') == "/users/profile") {
+    $("div.field_with_errors:has(label)").addClass("parent profile");
+    $("div.field_with_errors:has(input)").addClass("profile");
+    $("label").css("max-width","100%");
+    $("input").css("max-width","100%");
   }
 });
