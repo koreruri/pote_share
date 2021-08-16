5994045724684b1bad69f4620ab1d7970863f3ce
EC2 Default User <ec2-user@ip-172-31-15-221.ap-northeast-1.compute.internal>
2021-08-16 03:13:16 +0000

    Clean duplicate routing and fix pathes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,17 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
+
   root 'static_pages#home'
+  
+  resources :users, only: :create
   get 'users/sign_up', to: 'users#new'
   get 'users/account', to: 'users#show'
   get 'users/profile', to: 'users#profile'
   patch 'users/profile', to: 'users#profile_update'
   get 'users/edit', to: 'users#edit'
   patch 'users/edit', to: 'users#update'
+  
   get '/users/sign_in', to: 'sessions#new'
   post '/users/sign_in', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
-  resources :users
+  
 end
--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -34,7 +34,7 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     follow_redirect!
     assert_select "a[href=?]", users_sign_in_path
     assert_select "a[href=?]", logout_path, count: 0
-    assert_select "a[href=?]", user_path(@user), count: 0
+    assert_select "a[href=?]", users_account_path, count: 0
   end
   
   test "login with invalid information" do
