bd802bb5283e5a28653a746f9864881df85b20a6
EC2 Default User <ec2-user@ip-172-31-15-221.ap-northeast-1.compute.internal>
2021-08-16 04:47:40 +0000

    Add validation to room with tests

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,3 +1,8 @@
 class Room < ApplicationRecord
   belongs_to :user
+  validates :user_id, presence: true
+  validates :name, presence: true
+  validates :introduction, presence: true
+  validates :price, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :address, presence: true
 end
--- test/models/room_test.rb
+++ test/models/room_test.rb
@@ -1,7 +1,54 @@
 require "test_helper"
 
 class RoomTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  def setup
+    @user = users(:michael)
+    #このコードは慣習的に正しくない
+    @room = Room.new(name: "test room", introduction: "Lorem ipsum", price: 5000, address: "東京都新宿区", user_id: @user.id)
+  end
+  
+  test "should be valid" do
+    assert @room.valid?
+  end
+  
+  test "user id should be present" do
+    @room.user_id = nil
+    assert_not @room.valid?
+  end
+  
+  test "name should be present" do
+    @room.name = ""
+    assert_not @room.valid?
+  end
+  
+  test "introduction should be present" do
+    @room.introduction = ""
+    assert_not @room.valid?
+  end
+  
+  test "price should be present" do
+    @room.price = ""
+    assert_not @room.valid?
+  end
+  
+  test "price should be numbers" do
+    @room.price = "price"
+    assert_not @room.valid?
+  end
+  
+  test "price should be greater than 0" do
+    @room.price = -1000
+    assert_not @room.valid?
+  end
+  
+  test "price should be integer" do
+    @room.price = 1000.1
+    assert_not @room.valid?
+  end
+  
+  test "address should be present" do
+    @room.address=""
+    assert_not @room.valid?
+  end
 end
