d0ceddd7da402bb2425ab7b6e69495e63a028aa3
EC2 Default User <ec2-user@ip-172-31-15-221.ap-northeast-1.compute.internal>
2021-08-16 03:58:51 +0000

    Change users introduction data type

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_14_053048) do
+ActiveRecord::Schema.define(version: 2021_08_16_035549) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -46,7 +46,7 @@ ActiveRecord::Schema.define(version: 2021_08_14_053048) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
-    t.string "introduction"
+    t.text "introduction"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
