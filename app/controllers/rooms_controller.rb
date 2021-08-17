f9b435f8f10ee892c57d1f7ddabf61e68d7fbb40
EC2 Default User <ec2-user@ip-172-31-15-221.ap-northeast-1.compute.internal>
2021-08-17 08:08:24 +0000

    Add new create and image upload to rooms

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -2,9 +2,18 @@ class RoomsController < ApplicationController
   before_action :logged_in_user, only: [:new, :create]
   
   def new
+    @room = current_user.rooms.build if logged_in?
   end
   
   def create
+    @room = current_user.rooms.build(room_params)
+    @room.image.attach(params[:room][:image])
+    if @room.save
+      flash[:success] = "Room was successfully created."
+      redirect_to @room
+    else
+      render 'new'
+    end
   end
   
   def search
@@ -14,4 +23,9 @@ class RoomsController < ApplicationController
             .or(@rooms.where('introduction LIKE ?', "%#{params[:keyword]}%"))
             .or(@rooms.where('address LIKE ?', "%#{params[:keyword]}%")) if params[:keyword].present?
   end
+  
+  private
+    def room_params
+      params.require(:room).permit(:name, :introduction, :price, :address, :image)
+    end
 end
--- app/models/room.rb
+++ app/models/room.rb
@@ -1,9 +1,14 @@
 class Room < ApplicationRecord
   belongs_to :user
+  has_one_attached :image
   default_scope -> { order(created_at: :desc) }
   validates :user_id, presence: true
   validates :name, presence: true
   validates :introduction, presence: true
   validates :price, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :address, presence: true
+  validates :image, content_type: { in: %w[image/jpeg image/gif image/png],
+                        message: "jpeg,gif,pngファイルのいずれかを選択して下さい" },
+                    size: { less_than: 5.megabytes,
+                        message: "画像ファイルサイズは5MB以下にして下さい" }
 end
