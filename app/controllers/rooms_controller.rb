fcae1bcf4c3393d4e2f03e3c1f31de558d25c86e
EC2 Default User <ec2-user@ip-172-31-15-221.ap-northeast-1.compute.internal>
2021-08-18 03:04:14 +0000

    Fix rooms research with no word

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -22,10 +22,16 @@ class RoomsController < ApplicationController
   
   def search
     @rooms = Room.all
-    @results = @rooms.where('address LIKE ?', "%#{params[:area]}%") if params[:area].present?
-    @results = @rooms.where('name LIKE ?', "%#{params[:keyword]}%")
+    
+    if params[:area].present?
+      @results = @rooms.where('address LIKE ?', "%#{params[:area]}%")
+    elsif params[:keyword].present?
+      @results = @rooms.where('name LIKE ?', "%#{params[:keyword]}%")
             .or(@rooms.where('introduction LIKE ?', "%#{params[:keyword]}%"))
-            .or(@rooms.where('address LIKE ?', "%#{params[:keyword]}%")) if params[:keyword].present?
+            .or(@rooms.where('address LIKE ?', "%#{params[:keyword]}%"))
+    else
+      @results = @rooms
+    end
   end
   
   private
