6c55f6da4f0f3887b528af38e5330c0957f5c2e9
EC2 Default User <ec2-user@ip-172-31-15-221.ap-northeast-1.compute.internal>
2021-08-19 02:53:24 +0000

    Add index to reservations

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,5 +1,8 @@
 class ReservationsController < ApplicationController
+  before_action :logged_in_user, only: [:index, :new, :create]
+  
   def index
+    @reservations = current_user.reservations
   end
 
   def new
--- test/controllers/reservations_controller_test.rb
+++ test/controllers/reservations_controller_test.rb
@@ -1,13 +1,38 @@
 require "test_helper"
 
 class ReservationsControllerTest < ActionDispatch::IntegrationTest
+  
+  def setup
+    @user = users(:michael)
+  end
+  
   test "should get index" do
+    log_in_as(@user)
     get reservations_path
     assert_response :success
   end
 
   test "should get new" do
+    log_in_as(@user)
     get new_reservation_path
     assert_response :success
+  end
+  
+   test "should redirect index when not logged in" do
+    get reservations_path
+    assert_not flash.empty?
+    assert_redirected_to users_sign_in_url
+  end
+  
+  test "should redirect new when not logged in" do
+    get new_reservation_path
+    assert_not flash.empty?
+    assert_redirected_to users_sign_in_url
+  end
+  
+  test "should redirect update when not logged in" do
+    post reservations_path
+    assert_not flash.empty?
+    assert_redirected_to users_sign_in_url
   end
 end
