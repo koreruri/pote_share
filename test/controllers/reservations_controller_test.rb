7f251524e94070bb860a49e2d2af70bfe036c82e
EC2 Default User <ec2-user@ip-172-31-15-221.ap-northeast-1.compute.internal>
2021-08-19 02:05:08 +0000

    Fix reservations_controller test

--- test/controllers/reservations_controller_test.rb
+++ test/controllers/reservations_controller_test.rb
@@ -2,17 +2,12 @@ require "test_helper"
 
 class ReservationsControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get reservations_index_url
+    get reservations_path
     assert_response :success
   end
 
   test "should get new" do
-    get reservations_new_url
-    assert_response :success
-  end
-
-  test "should get create" do
-    get reservations_create_url
+    get new_reservation_path
     assert_response :success
   end
 end
