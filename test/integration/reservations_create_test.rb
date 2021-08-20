313418ae2e617ec09c2b74fc14df661b2944e265
EC2 Default User <ec2-user@ip-172-31-15-221.ap-northeast-1.compute.internal>
2021-08-20 08:02:24 +0000

    Add successful reservation create test

---
+++ test/integration/reservations_create_test.rb
@@ -0,0 +1,26 @@
+require "test_helper"
+
+class ReservationsCreateTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @user = users(:michael)
+    @room = rooms(:sapporo)
+  end
+  
+  test "successful reservation create" do
+    log_in_as(@user)
+    start_date = Date.today
+    end_date = Date.today + 1
+    assert_difference 'Reservation.count', 1 do
+      post reservations_path, params: { reservation: { room_id: @room.id,
+                                                       start_date: start_date,
+                                                       end_date: end_date,
+                                                       person_num: 2,
+                                                       total_price: 12000} }
+    end
+    assert_not flash.empty?
+    reservation = assigns(:reservation)
+    assert_redirected_to reservation
+  end
+  
+end
