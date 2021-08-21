068dcd286679131426fe9e0392d04b9737c98346
EC2 Default User <ec2-user@ip-172-31-15-221.ap-northeast-1.compute.internal>
2021-08-21 08:33:51 +0000

    Add css and jQuery to rooms new when it fails

--- app/javascript/main.js
+++ app/javascript/main.js
@@ -7,5 +7,12 @@ $(function() {
     $("div.field_with_errors:has(input)").addClass("profile");
     $("label").css("max-width","100%");
     $("input").css("max-width","100%");
+  } else if ($(location).attr('pathname') == "/rooms/new") {
+    $("div.field_with_errors:has(label)").addClass("parent room_new");
+    $("div.field_with_errors:has(input)").addClass("room_new");
+    $("div.field_with_errors:has(textarea)").addClass("room_new");
+    $("label").css("max-width","100%");
+    $("input").css("max-width","100%");
+    $("textarea").css("max-width","100%");
   }
 });
