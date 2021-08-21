5262433c450cb49968a83f928f30f87e248ddcdc
EC2 Default User <ec2-user@ip-172-31-15-221.ap-northeast-1.compute.internal>
2021-08-21 07:23:03 +0000

    Add redirect when incorrect state to sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,7 @@
 class SessionsController < ApplicationController
+  before_action :redirect_logged_in_user, only: [:new, :create]
+  before_action :redirect_not_logged_in, only: :destroy
+  
   def new
   end
   
@@ -18,5 +21,25 @@ class SessionsController < ApplicationController
     log_out
     flash[:success] = "Signed out successfully."
     redirect_to root_url
+  end
+  
+  private
+
+  # beforeアクション
+  
+  #ログイン済みの場合はリダイレクト
+  def redirect_logged_in_user
+    if logged_in?
+      flash[:danger] = "You are already signed in."
+      redirect_to root_url
+    end
+  end
+  
+  #ログインしていない場合はリダイレクト
+  def redirect_not_logged_in
+    unless logged_in?
+      flash[:danger] = "You are already logged out."
+      redirect_to root_url
+    end
   end
 end
